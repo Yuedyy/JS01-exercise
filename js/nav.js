fd7dfb75e68a8e932d21d0674a22b4264dc6ff53
栗昂桀 <liangjie.yuedyy@gmail.com>
2015-09-09 23:49:27 +0800

    好像元素选择可以了，但是还不可以用。。。

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,10 @@
 window.onload=function()
 {
-	var oCont=document.getElementById('container');
-	var oPart=oCont.getElementById('part');
-	var aNav=oPart.getElementById('nav');
-	var aUl_nav1=aNav.getElementsByName('nav-1')[0];
-	var aTxt_nav1=aNav.getElementsByName('nav-text');
+	var aNav=document.getElementById('nav');
+	var aUl_nav1=aNav.getElementsByClassName('nav-1')[0];
+	var aTxt_nav1=aNav.getElementsByClassName('nav-text');
 
-	var aUl_nav2=aUl_nav1.getElementsByName('nav-2')[0];
+	var aUl_nav2=aUl_nav1.getElementsByClassName('nav-2')[0];
 	var aBtn_nav2=aUl_nav2.getElementsByTagName('li');
 	
 	for(var i=0; i<aBtn_nav2.length; i++)
