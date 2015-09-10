4f87d4897aaf509e83c9b0c800bb2e66c383346e
栗昂桀 <liangjie.yuedyy@gmail.com>
2015-09-10 22:48:41 +0800

    js编写中还是出现了问题，CSS非行间样式获取不到

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,15 @@
+//选择非行间css样式
+function getStyle(obj,name){
+	if(obj.currentStyle)
+	{
+		return obj.currentStyle[name];
+	}
+	else{
+		return getComputedStyle(obj,false)[name];
+	}
+}
+
+
 window.onload=function()
 {
 	var aNav=document.getElementById('nav');
@@ -7,18 +19,22 @@ window.onload=function()
 	var aUl_nav2=aUl_nav1.getElementsByClassName('nav-2')[0];
 	var aBtn_nav2=aUl_nav2.getElementsByTagName('li');
 	
+
 	for(var i=0; i<aBtn_nav2.length; i++)
 	{
 		aBtn_nav2[i].index=i;
 		aBtn_nav2[i].onmouseover=function()
 		{
+			
 			for(var i=0; i<aBtn_nav2.length; i++)
 			{
 				aBtn_nav2[i].className=' ';
-				aTxt_nav1[i].style.display='none'
+				getStyle(aTxt_nav1[i],z-index)=1;
+				
 			}
 			this.className="active";
-			aTxt_nav1[this.index].style.display="block";
+			getStyle(aTxt_nav1[this.index],z-index)=4000;
+			
 			
 		}
 		aBtn_nav2[i].onmousemove=function()
@@ -27,7 +43,7 @@ window.onload=function()
 			{
 				aBtn_nav2[i].className=' ';
 			}
-			aTxt_nav1[this.index].style.display="none";
+			getStyle(aTxt_nav1[this.index],z-index)=1;
 		}
 	}
 }
